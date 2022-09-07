59971e966f8fd22baa7e18537b7a1333ca0d5b35
kiisi <kiisifelix06@gmail.com>
2022-09-07 12:52:35 +0100

    Accordion updates

--- src/components/Home/Experience/Experience.js
+++ src/components/Home/Experience/Experience.js
@@ -25,7 +25,7 @@ const Experience = () => {
                                     With the help of Lyft team I had to design and implement software to create new rental fleet in an effort to promote more sustainable, linked cities across the US.
                                     <h2>Organization:</h2>
                                     I had to make sure all my code are organized and commented so that my partners can use. And that code are useable and more readable by my fellow coders or whoever will reference my code later on.
-                                    <h2>Time Frame</h2>
+                                    <h2>Time Frame:</h2>
                                     8 months
                                     Jan 2022 - Aug 2022
                                     Volunteering
@@ -40,11 +40,11 @@ const Experience = () => {
                             </h2>
                             <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
                                 <div className="accordion-body">
-                                    <h2>Learning</h2>
+                                    <h2>Learning:</h2>
                                     I had to consistently develop new skills. At first I was only good at Web Development but then I had to learn new skills in application layers, presentation layers, databases and more.
-                                    <h2>Problem Solving</h2>
+                                    <h2>Problem Solving:</h2>
                                     I had to use my problem solving skills a lot to use perspective of JP Morgan Chase to learn from Stock data and set up a system for analysis of the data for their traders.
-                                    <h2>Resilience</h2>
+                                    <h2>Resilience:</h2>
                                     I got stuck a lot of times but I always made sure to keep going and keep pushing because I always looked the end goal and how great it would feel to see the end product.
                                     <h2>Time Frame:</h2>
                                     1 year 2 months
@@ -60,15 +60,15 @@ const Experience = () => {
                             </h2>
                             <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
                                 <div className="accordion-body">
-                                    <h2>Problem Solving</h2>
+                                    <h2>Problem Solving:</h2>
                                     I used my problem solving skills to examine data to find crucial consumer insights and potential uses for the information in problem-solving.
-                                    <h2>Communication</h2>
+                                    <h2>Communication:</h2>
                                     I had to communicate with clients to understand user needs and then propose those idea to developers to better the software.
-                                    <h2>Teamwork</h2>
+                                    <h2>Teamwork:</h2>
                                     Team work was super important for this job, I had to report my understanding of different parts of the software with different team to better the software. Analyzes user needs and software requirements to inform system design
-                                    <h2>Learning</h2>
+                                    <h2>Learning:</h2>
                                     Learning was a major part of this job, I had to continuous study and learn new technologies. I had to communicate with different level of leadership to understand and learn more to better the technology.
-                                    <h2>Time Frame</h2>
+                                    <h2>Time Frame:</h2>
                                     1 year 6 months
                                     Mar 2021 - Aug 2022
                                 </div>
@@ -82,13 +82,13 @@ const Experience = () => {
                             </h2>
                             <div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
                                 <div className="accordion-body">
-                                    <h2>Communication</h2>
+                                    <h2>Communication:</h2>
                                     I ran and operated this business, so I had to deal with many different types of customers from many different type of backgrounds. It was difficult because I had to learn how different customer might react and how to read them beforehand. I also had to learn sale to a great extend.
-                                    <h2>Leadership</h2>
+                                    <h2>Leadership:</h2>
                                     This position required massive amount of leadership skills that I gain by myself using trial and error, for instance learning how to hire slowly and fire quickly, its a hard job but after some time you get used to it.
-                                    <h2>Problem Solving</h2>
+                                    <h2>Problem Solving:</h2>
                                     Since I was CIO or basically the guy who did all the tech work in the company, I had to solve many problems myself, for instance making an amazing website for the company, setting up clover payment system so that the customer can pay with cards and we can manage staff with it.
-                                    <h2>Learning</h2>
+                                    <h2>Learning:</h2>
                                     I had to learn how to setup and use POS all by myself just using some youtube videos, I had to learn how to use excel to make key business decision for my business. All these took massive amount of learning which took a lot of hard work and of course patience.
                                 </div>
                             </div>
