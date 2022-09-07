1d23b7505d321ca463f1a01fc3a27f3749b51fee
kiisi <kiisifelix06@gmail.com>
2022-09-07 22:46:13 +0100

    Adding mobile Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React,{useState, useRef} from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+
+
+  const [click, setClick] = useState(false)
+  const mobileNavRef = useRef()
+  const mobileNavCloseRef = useRef()
+  
+  const menuHandler = () =>{
+    setClick(!click)
+    let mobileNav = mobileNavRef.current
+    let mobileNavClose = mobileNavCloseRef.current
+
+
+    mobileNav.classList.remove("nav-slide-effect-out")
+    mobileNav.classList.add("nav-slide-effect-in")
+    mobileNav.addEventListener('animationend',()=>{
+      mobileNav.style.right = '0';
+      mobileNavClose.classList.add('show')
+    })
+
+    
+    
+  }
+
+  const closeHandler = () =>{
+    setClick(!click)
+    let mobileNav = mobileNavRef.current
+    let mobileNavClose = mobileNavCloseRef.current
+
+
+    mobileNavClose.classList.remove('show')
+
+    mobileNav.classList.remove("nav-slide-effect-in")
+    mobileNav.classList.add("nav-slide-effect-out")
+    mobileNav.addEventListener('animationend',()=>{
+      mobileNav.style.right = '-40rem';
+      mobileNavClose.classList.remove('show')
+
+    })
+  }
+
   return (
+    <>
     <div className="max-wrapper">
         <nav className="nav">
             <div className="nav-header">Fahmid<span className="material-icons">bolt</span></div>
@@ -12,11 +53,33 @@ const Navbar = () => {
                 <div className="nav-links-item">Skills</div>
                 <div className="nav-links-item">Contact</div>
             </div>
-            <div className="nav-menu-icon">
+            <div className="nav-menu-icon" onClick={menuHandler}>
             <span className="material-icons">menu</span>
             </div>
         </nav>
     </div>
+
+    <nav className="mobile-nav" ref={mobileNavRef}>
+        <div className="mobile-nav-close-wrapper">
+          <div className="mobile-nav-close" onClick={closeHandler} ref={mobileNavCloseRef}>
+            <span className="material-icons">close</span>
+          </div>
+        </div>
+        <div className="mobile-nav-content">
+          <div className="nav-header mobile-nav-header">
+            <h2><span>Fast</span>Food</h2>
+          </div>
+          <div className="mobile-nav-menu">
+              <div>Home</div>
+              <div>Menu</div>
+              <div>Service</div>
+              <div>Shop</div>
+            </div>
+        </div>
+      </nav>
+      <div className="nav-modal"style={{display: click ? 'block' : 'none'}} onClick={closeHandler}></div>
+
+    </>
   )
 }
 
