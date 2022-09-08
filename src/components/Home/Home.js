79ce1b456ae4178540dfb5ce9e4186a7723a3c3e
kiisi <kiisifelix06@gmail.com>
2022-09-08 07:41:06 +0100

    Rundown removed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import About from './About/About'
 import Connect from './Connect/Connect'
 import Hero from './Hero/Hero'
-import RunDown from './RunDown/RunDown'
 import Skills from './Skills/Skills'
 import Projects from './Projects/Projects'
 import Skillset from './Skillset/Skillset'
@@ -13,7 +12,6 @@ const Home = () => {
     <>
         <Hero/>
         <Skills/>
-        <RunDown/>
         <About/>
         <Projects/>
         <Skillset/>
